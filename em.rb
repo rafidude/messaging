81135b56f72e8ff835edc8d533db5bfee7519ba4
Rafi Dudekula <rafi.dudekula@gmail.com>
2011-07-12 15:57:03 -0500

    Added timing info

--- em.rb
+++ em.rb
@@ -2,6 +2,8 @@ require 'em-redis'
 require './db'
 EM.run do
   EM.add_periodic_timer(2) do
+    t1 = Time.now
+    # processing...
     puts "Tick... every 2 secs"
     fetch = true
     count = -1
@@ -11,11 +13,14 @@ EM.run do
       fetch = false unless message_key
       count += 1
     end while fetch
+    t2 = Time.now
+    delta = t2 - t1
     puts "completed processing #{count} messages" if count > 0
+    puts delta if count > 0
   end
   
-  EM.add_periodic_timer(5) do
-    puts "Every 5 secs Tick..."
+  EM.add_periodic_timer(10) do
+    puts "Every 10 secs Tick..."
     # Fetch latest user subscriptions from SQL and cache the data in redis
     # DBSQL.get_user_subscriptions
     DB.populate_channels_with_subscribed_users
